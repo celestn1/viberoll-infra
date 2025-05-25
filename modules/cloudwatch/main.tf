e0bb3dbcfec19cf845bf57928f4569c1096982ef
Celestine Ntadi <contactilec@gmail.com>
2025-05-25 18:03:32 +0100

    Deploy Viberoll-Backend AWS infrastructure

--- modules/cloudwatch/main.tf
+++ modules/cloudwatch/main.tf
@@ -3,7 +3,7 @@
 # ------------------------------
 resource "aws_cloudwatch_log_group" "ecs_logs" {
   name              = var.log_group_name
-  retention_in_days = 1 # Keep logs only for 1 day to stay within Free Tier
+  retention_in_days = 1 # Keep logs only for 1 day to stay within Free Tier 14 days
 
   tags = {
     Project     = var.project_name
